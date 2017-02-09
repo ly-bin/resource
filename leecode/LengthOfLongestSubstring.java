8441da7bd49dac5a25a847f5bafa3afde8013eae
悬崖的鹰 <qtsyouxiang@163.com>
2017-02-09 18:08:20 +0800

    update a file

--- leecode/LengthOfLongestSubstring.java
+++ leecode/LengthOfLongestSubstring.java
@@ -1,7 +1,7 @@
 /**
  * @author qb
  * @description: 向后遍历数组，遇到重复值停止，比较最大长度（最小为1），
- *				 下次从重复值的下个位置进行遍历，直到字符串遍历完毕。
+ *               下次从重复值的下个位置进行遍历，直到字符串遍历完毕。
  * @date 2017-2-9 17:28
  */
 public class LengthOfLongestSubstring {
@@ -16,7 +16,7 @@ public class LengthOfLongestSubstring {
         }
         int start = 0;
         int maxLen = 1;
-		// 大小写字母的ASCII值
+	    // 大小写字母的ASCII值
         boolean [] arr = new boolean[128];
         int len = s.length();
         for (int i = 0;i < len;i++){
